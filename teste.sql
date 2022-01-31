99a69b6138bd23ddc229ef80fe68ef0104766b0e
Vini Santos <vini_santos7@live.com>
2022-01-31 16:48:32 -0300

    add a node.js in the project, pull for my local

---
+++ teste.sql
@@ -0,0 +1,11 @@
+create table voluntarios(
+  nome varchar(50),
+  email varchar(100),
+  celular int,
+);
+
+insert into usuario(nome, email, celular) values (
+   "Marjorie Santos"
+   "marjorie@teste.com",
+   1198888-8888
+);
